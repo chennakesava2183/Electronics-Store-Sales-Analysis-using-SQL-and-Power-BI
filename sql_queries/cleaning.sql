75da51a860a3df450e32753813f128e573e1e5ea
Chenna Kesava Kumar Bonu <thisischenna@gmail.com>
2026-02-07 20:04:20 +0530

    Add files via upload

---
+++ sql_queries/cleaning.sql
@@ -0,0 +1,9 @@
+INSERT INTO information_schema.columns (column_name,data_type,character_maximum_length,numeric_precision,numeric_scale,is_nullable) VALUES
+	 ('ordernumber','integer',NULL,32,0,'YES'),
+	 ('lineitem','integer',NULL,32,0,'YES'),
+	 ('orderdate','date',NULL,NULL,NULL,'YES'),
+	 ('customerkey','integer',NULL,32,0,'YES'),
+	 ('storekey','integer',NULL,32,0,'YES'),
+	 ('productkey','integer',NULL,32,0,'YES'),
+	 ('quantity','integer',NULL,32,0,'YES'),
+	 ('currencycode','character varying',50,NULL,NULL,'YES');
